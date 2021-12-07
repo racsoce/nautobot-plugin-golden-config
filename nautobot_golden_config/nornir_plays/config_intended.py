eea93bd4ad717552c635a9ba6e3f75c664bda5b8
netengauto <ocarrillo@contractor.ea.com>
2021-12-07 10:31:19 -0600

    setup netmiko extras

--- nautobot_golden_config/nornir_plays/config_intended.py
+++ nautobot_golden_config/nornir_plays/config_intended.py
@@ -124,6 +124,10 @@ def config_intended(nautobot_job, data, jinja_root_path, intended_root_folder):
             },
         ) as nornir_obj:
 
+            for dev in nornir_obj.inventory.hosts.keys():
+                """ setup netmiko extras per host in the inventory """
+                nornir_obj.inventory.hosts[dev].connection_options['netmiko'].extras = NORNIR_SETTINGS.get("netmiko_extras")
+
             nr_with_processors = nornir_obj.with_processors([ProcessGoldenConfig(logger)])
 
             # Run the Nornir Tasks
